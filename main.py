b8f4dd0485886dde44f8993ee967da39968aeddd
gonle11 <251201961+gonle11@users.noreply.github.com>
2026-01-19 12:27:26 +0100

    Refactor plant initialization for simulation
    Removed hardcoded plant instances and replaced with a list of available plants for simulation.

--- main.py
+++ main.py
@@ -2,39 +2,22 @@ from plant import Plant
 from simulation import Simulation
 import time
 
-# have to finish to complete the caracteristics of each a plant and define light, hmuidity's,  warmth's, range
-plant1 = Plant(
-    name="Cactus",
-    water=30,
-    light=80,
-    humidity=20,
-    optimal_water=(20, 40),
-    optimal_light=(70, 100),
-    optimal_humidity=(10, 30)
+available_plants = [
+    create_Epipremnum_aureum(), 
+]
+
+simulation = Simulation(
+    plants=[],
+    available_plants=available_plants
 )
 
-plant2 = Plant(
-    name="Fern",
-    water=60,
-    light=40,
-    humidity=70,
-    optimal_water=(50, 80),
-    optimal_light=(30, 60),
-    optimal_humidity=(60, 90)
-)
-
-
-simulation = Simulation([plant1, plant2])
-
-
-
-simulation = Simulation([plant1, plant2])
 
 while simulation.running:
     simulation.update()
+    
 
     print(f"Time: {simulation.time}")
-    for i, plant in enumerate(simulation.plants):
+    for i, plant in enumerate(simulation.plants):   # affiche les infos de chaque plantes à relier avec interface
         print(
             f"Plant {i+1} | "
             f"Health: {plant.health} | "
