2783de56edd6aa3c0e05b23f7f0dd5a527fd6d71
gonle11 <251201961+gonle11@users.noreply.github.com>
2026-01-19 11:55:41 +0100

    Implement plant creation functions for various species
    Added functions to create various plant instances with specific attributes.

--- plant_class.py
+++ plant_class.py
@@ -26,19 +26,189 @@ class Plant:
         self.growth_stage = 1
         self.age = 0
 
+
+    # easy plants
+    def create_Epipremnum_aureum():
+        return Plant(
+            name="Epipremnum aureum",
+            water=50,
+            light=55,
+            humidity=50,
+            warmth=55,
+            optimal_water=(40, 60),
+            optimal_light=(40, 70),
+            optimal_humidity=(40, 60),
+            optimal_warmth=(45, 65)
+        )
+    
+    
+    def create_Sansevieria_trifasciata():
+        return Plant(
+            name="Sansevieria trifasciata",
+            water=40,
+            light=55,
+            humidity=30,
+            warmth=55,
+            optimal_water=(30, 50),
+            optimal_light=(30, 80),
+            optimal_humidity=(20, 40),
+            optimal_warmth=(45, 65)
+        )
+    
+    
+    def create_Chlorophytum_comosum():
+        return Plant(
+            name="Chlorophytum comosum",
+            water=55,
+            light=65,
+            humidity=50,
+            warmth=55,
+            optimal_water=(45, 65),
+            optimal_light=(50, 80),
+            optimal_humidity=(40, 60),
+            optimal_warmth=(50, 65)
+        )
+
+    # Medium plants
+    def create_Ficus_elastica():
+        return Plant(
+            name="Ficus elastica",
+            water=60,
+            light=70,
+            humidity=60,
+            warmth=60,
+            optimal_water=(50, 70),
+            optimal_light=(60, 80),
+            optimal_humidity=(50, 70),
+            optimal_warmth=(55, 70)
+        )
+    
+    
+    def create_Monstera_deliciosa():
+        return Plant(
+            name="Monstera deliciosa",
+            water=65,
+            light=70,
+            humidity=70,
+            warmth=65,
+            optimal_water=(55, 75),
+            optimal_light=(60, 80),
+            optimal_humidity=(60, 80),
+            optimal_warmth=(60, 75)
+        )
+    
+    
+    def create_Spathiphyllum_wallisii():
+        return Plant(
+            name="Spathiphyllum wallisii",
+            water=60,
+            light=50,
+            humidity=80,
+            warmth=60,
+            optimal_water=(50, 70),
+            optimal_light=(40, 60),
+            optimal_humidity=(70, 90),
+            optimal_warmth=(55, 70)
+        )
+    
+    
+    def create_dracaena_marginata():
+        return Plant(
+            name="Dracaena marginata",
+            water=55,
+            light=60,
+            humidity=50,
+            warmth=60,
+            optimal_water=(45, 65),
+            optimal_light=(50, 70),
+            optimal_humidity=(40, 60),
+            optimal_warmth=(55, 70)
+        )
         
-def is_in_optimal_range(self):
-    return (
-        self.optimal_water[0] <= self.water <= self.optimal_water[1] and
-        self.optimal_light[0] <= self.light <= self.optimal_light[1] and
-        self.optimal_humidity[0] <= self.humidity <= self.optimal_humidity[1]
+# hard plant
+    def create_Calathea_orbifolia():
+    return Plant(
+        name="Calathea orbifolia",
+        water=65,
+        light=50,
+        humidity=90,
+        warmth=65,
+        optimal_water=(60, 70),
+        optimal_light=(40, 60),
+        optimal_humidity=(80, 100),
+        optimal_warmth=(60, 70)
     )
 
+
+def create_Alocasia_amazonica():
+    return Plant(
+        name="Alocasia amazonica",
+        water=70,
+        light=70,
+        humidity=90,
+        warmth=70,
+        optimal_water=(65, 75),
+        optimal_light=(60, 80),
+        optimal_humidity=(80, 100),
+        optimal_warmth=(65, 75)
+    )
+
+    
+    def create_Ficus_lyrata():
+        return Plant(
+            name="Ficus lyrata",
+            water=60,
+            light=80,
+            humidity=60,
+            warmth=65,
+            optimal_water=(55, 65),
+            optimal_light=(70, 90),
+            optimal_humidity=(50, 70),
+            optimal_warmth=(60, 70)
+        )
+#specilal
+    def create_Cactus_ferocactus():
+    return Plant(
+        name="Cactus ferocactus",
+        water=25,
+        light=95,
+        humidity=15,
+        warmth=80,
+        optimal_water=(20, 40),
+        optimal_light=(90, 100),
+        optimal_humidity=(10, 20),
+        optimal_warmth=(75, 90)
+    )
+
+
+def create_Nepenthes_alata():
+    return Plant(
+        name="Nepenthes alata",
+        water=75,
+        light=80,
+        humidity=95,
+        warmth=75,
+        optimal_water=(70, 80),
+        optimal_light=(70, 90),
+        optimal_humidity=(90, 100),
+        optimal_warmth=(70, 80)
+    )
+
+
+    
+    
+    def is_in_optimal_range(self):
+        return (
+            self.optimal_water[0] <= self.water <= self.optimal_water[1] and
+            self.optimal_light[0] <= self.light <= self.optimal_light[1] and
+            self.optimal_humidity[0] <= self.humidity <= self.optimal_humidity[1]
+        )
+
         
-def get_water_decay(self):
+    def get_water_decay(self):
         return 1 + self.growth_stage * 0.5
 
-        def update(self):
+    def update(self):
         if not self.alive:
             return
 
@@ -70,6 +240,7 @@ def get_water_decay(self):
         if self.health <= 0:
             self.alive = False
 
+    
 
 
         
