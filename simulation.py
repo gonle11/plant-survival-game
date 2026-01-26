73a681e50b2f009e476e71531b7fa561a467d442
gonle11 <251201961+gonle11@users.noreply.github.com>
2026-01-26 12:29:11 +0100

    Refactor Simulation class to use plant_pool

--- simulation.py
+++ simulation.py
@@ -1,33 +1,47 @@
 import random
 
 class Simulation:
-    def __init__(self, plants, available_plants):
+    def __init__(self, plants, plant_pool):
         self.plants = plants
-        self.available_plants = available_plants
+        self.plant_pool = plant_pool
         self.time = 0
         self.running = True
+        #self.spawn_start = 5     # earliest time
+        #self.spawn_end = 60      # latest time
+        
+        self.spawn_probability = 0.1
 
-        self.spawn_start = 5     # earliest time
-        self.spawn_end = 60      # latest time
-        self.spawn_probability = 0.1  # 10% chance per tick
 
-    
-    def update(self):
-        self.time += 1
-    
-        # Random plant addition
-        if (
-            self.spawn_start <= self.time <= self.spawn_end
-            and self.available_plants
-            and random.random() < self.spawn_probability
-        ):
-            new_plant = random.choice(self.available_plants)
-            self.available_plants.remove(new_plant)
-            self.plants.append(new_plant)
-    
-        # Update existing plants
-        for plant in self.plants:
-            plant.update()
-            if not plant.alive:
-                self.running = False
+
+        
+        def update(self):
+            self.time += 1
+        
+            # Decide which difficulties are allowed
+            if self.time < 30:
+                allowed = ["easy"]
+            elif self.time < 60:
+                allowed = ["easy", "medium"]
+            else:
+                allowed = ["medium", "hard"]
+        
+            # Random spawn
+            if random.random() < self.spawn_probability:
+                possible = [
+                    plant
+                    for level in allowed
+                    for plant in self.plant_pool[level]
+                ]
+        
+                if possible:
+                    new_plant = random.choice(possible)
+                    self.plant_pool[new_plant.difficulty].remove(new_plant)
+                    self.plants.append(new_plant)
+        
+            # Update plants
+            for plant in self.plants:
+                plant.update()
+                if not plant.alive:
+                    self.running = False
+
     
