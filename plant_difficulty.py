40b5c116fe5b298ee9fc6cb8286619ec4702af17
gonle11 <251201961+gonle11@users.noreply.github.com>
2026-01-26 11:18:12 +0100

    Add plant difficulty categorization
    create pool of sorting plant by dfficlty

---
+++ plant_difficulty.py
@@ -0,0 +1,27 @@
+from plant import Plant
+
+plant_pool = {
+    "easy": [
+        Plant.create_Epipremnum_aureum(),
+        Plant.create_Sansevieria_trifasciata(),
+        Plant.create_Chlorophytum_comosum(),
+    ],
+
+    "medium": [
+        Plant.create_Ficus_elastica(),
+        Plant.create_Monstera_deliciosa(),
+        Plant.create_Spathiphyllum_wallisii(),
+        Plant.create_dracaena_marginata(),
+    ],
+
+    "hard": [
+        Plant.create_Calathea_orbifolia(),
+        Plant.create_Alocasia_amazonica(),
+        Plant.create_Ficus_lyrata(),
+    ],
+
+    "special": [
+        Plant.create_Cactus_ferocactus(),
+        Plant.create_Nepenthes_alata(),
+    ]
+}
