42c0188afee34b3c4f1328e3fa9d9dfff614ff35
gonle11 <251201961+gonle11@users.noreply.github.com>
2026-02-02 12:20:17 +0100

    Refactor update method and improve difficulty logic

--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
 import random
+import csv
 
 class Simulation:
     def __init__(self, plants, plant_pool):
@@ -14,34 +15,44 @@ class Simulation:
 
 
         
-        def update(self):
-            self.time += 1
+    def update(self):
+        self.time += 1
+
+        # define if level up
+        if self.age % 40 == 0 and self.level < 3:
+            self.level += 1
+
+    
+        # Decide which difficulties are allowed
+        if self.time < 30:
+            allowed = ["easy"]
+        elif self.time < 60:
+            allowed = ["easy", "medium"]
+        elif self.time < 75:
+            allowed = ["medium", "hard"]
+        else:
+            allowed = ["medium", "hard", "special"]
+
         
-            # Decide which difficulties are allowed
-            if self.time < 30:
-                allowed = ["easy"]
-            elif self.time < 60:
-                allowed = ["easy", "medium"]
-            else:
-                allowed = ["medium", "hard"]
-        
-            # Random spawn
-            if random.random() < self.spawn_probability:
-                possible = [
-                    plant
-                    for level in allowed
-                    for plant in self.plant_pool[level]
-                ]
-        
-                if possible:
-                    new_plant = random.choice(possible)
-                    self.plant_pool[new_plant.difficulty].remove(new_plant)
-                    self.plants.append(new_plant)
-        
-            # Update plants
-            for plant in self.plants:
-                plant.update()
-                if not plant.alive:
-                    self.running = False
+        # Random spawn
+        if random.random() < self.spawn_probability:
+            possible = [
+                plant
+                for level in allowed
+                for plant in self.plant_pool[level]
+            ]
+    
+            if possible:
+                new_plant = random.choice(possible)
+                self.plant_pool[new_plant.difficulty].remove(new_plant)
+                self.plants.append(new_plant)
+    
+        # Update plants
+        for plant in self.plants:
+            plant.update()
+            if not plant.alive:
+                self.running = False
+
+
 
     
