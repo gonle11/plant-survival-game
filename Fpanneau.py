d7dbde52dd729166c739265b3c3159a1786e8e4e
Nathan <nathan.temimevoisin@gmail.com>
2026-02-08 13:35:30 +0100

    mettre fonctions sur 6 premiers btn

--- Fpanneau.py
+++ Fpanneau.py
@@ -11,7 +11,7 @@ def panneau(fenetre):
     
     #plante 1
     def appel_plante1():
-        Fplantes.plante1(fenetre)
+        Fplantes.plante1(Pan)
     canvaPan.plante1=PhotoImage(file="plante1.png")
     btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante1,command=appel_plante1)
     btn1.place(x=75,y=75)
@@ -19,77 +19,77 @@ def panneau(fenetre):
 
     #plante 2
     def appel_plante2():
-        Fplantes.plante2(fenetre)
+        Fplantes.plante2(Pan)
     canvaPan.plante2=PhotoImage(file="plante1.png")
     btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante2,command=appel_plante2)
     btn1.place(x=215,y=75)
 
     #plante 3
     def appel_plante3():
-        Fplantes.plante3(fenetre)
+        Fplantes.plante3(Pan)
     canvaPan.plante3=PhotoImage(file="plante1.png")
     btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante3, command=appel_plante3)
     btn1.place(x=355,y=75)
 
     #plante 4
     def appel_plante4():
-        Fplantes.plante4(fenetre)
+        Fplantes.plante4(Pan)
     canvaPan.plante4=PhotoImage(file="plante1.png")
     btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante4,command=appel_plante4)
     btn1.place(x=495,y=75)
 
     #plante 5
     def appel_plante5():
-        Fplantes.plante5(fenetre)
+        Fplantes.plante5(Pan)
     canvaPan.plante5=PhotoImage(file="plante1.png")
     btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante5,command=appel_plante5)
     btn1.place(x=75,y=200)
 
     #plante 6
     def appel_plante6():
-        Fplantes.plante6(fenetre)
+        Fplantes.plante6(Pan)
     canvaPan.plante6=PhotoImage(file="plante1.png")
     btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante6,command=appel_plante6)
     btn1.place(x=215,y=200)
 
     #plante 7
     def appel_plante7():
-        Fplantes.plante7(fenetre)
+        Fplantes.plante7(Pan)
     canvaPan.plante7=PhotoImage(file="plante1.png")
     btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante7,command=appel_plante7)
     btn1.place(x=355,y=200)
 
     #plante 8
     def appel_plante8():
-        Fplantes.plante8(fenetre)
+        Fplantes.plante8(Pan)
     canvaPan.plante8=PhotoImage(file="plante1.png")
     btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante8,command=appel_plante8)
     btn1.place(x=495,y=200)
 
     #plante 9
     def appel_plante9():
-        Fplantes.plante9(fenetre)
+        Fplantes.plante9(Pan)
     canvaPan.plante9=PhotoImage(file="plante1.png")
     btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante9,command=appel_plante9)
     btn1.place(x=75,y=325)
 
     #plante 10
     def appel_plante10():
-        Fplantes.plante10(fenetre)
+        Fplantes.plante10(Pan)
     canvaPan.plante10=PhotoImage(file="plante1.png")
     btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante10,command=appel_plante10)
     btn1.place(x=215,y=325)
 
     #plante 11
     def appel_plante11():
-        Fplantes.plante11(fenetre)
+        Fplantes.plante11(Pan)
     canvaPan.plante11=PhotoImage(file="plante1.png")
     btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante11,command=appel_plante11)
     btn1.place(x=355,y=325)
     
     #plante 12
     def appel_plante12():
-        Fplantes.plante12(fenetre)
+        Fplantes.plante12(Pan)
     canvaPan.plante12=PhotoImage(file="plante1.png")
     btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante12,command=appel_plante12)
     btn1.place(x=495,y=325)
--- Fplantes.py
+++ Fplantes.py
@@ -1,37 +1,73 @@
 from tkinter import *
 
-def plante1(fenetre):
+def plante1(Pan):
     print("plante 1")
-    
-def plante2(fenetre):
+    plt1=Toplevel(Pan)
+    canva1=Canvas(plt1,width=300, height=200)
+    canva1.bg=PhotoImage(file="bgpanneau.png")
+    canva1.create_image(150,100,image=canva1.bg)
+    canva1.create_text(150,100,text="Plante 1")
+    canva1.pack()
+
+def plante2(Pan):
     print("plante 2")
+    plt2=Toplevel(Pan)
+    canva2=Canvas(plt2,width=300, height=200)
+    canva2.bg=PhotoImage(file="bgpanneau.png")
+    canva2.create_image(150,100,image=canva2.bg)
+    canva2.create_text(150,100,text="Plante 2")
+    canva2.pack()
 
-def plante3(fenetre):
+def plante3(Pan):
     print("plante 3")
+    plt3=Toplevel(Pan)
+    canva3=Canvas(plt3,width=300, height=200)
+    canva3.bg=PhotoImage(file="bgpanneau.png")
+    canva3.create_image(150,100,image=canva3.bg)
+    canva3.create_text(150,100,text="Plante 3")
+    canva3.pack()
     
-def plante4(fenetre):
+def plante4(Pan):
     print("plante 4")
-    
-def plante5(fenetre):
-    print("plante 5")
-    
-def plante6(fenetre):
-    print("plante 6")
+    plt4=Toplevel(Pan)
+    canva4=Canvas(plt4,width=300, height=200)
+    canva4.bg=PhotoImage(file="bgpanneau.png")
+    canva4.create_image(150,100,image=canva4.bg)
+    canva4.create_text(150,100,text="Plante 4")
+    canva4.pack()
 
-def plante7(fenetre):
+def plante5(Pan):
+    print("plante 5")
+    plt5=Toplevel(Pan)
+    canva5=Canvas(plt5,width=300, height=200)
+    canva5.bg=PhotoImage(file="bgpanneau.png")
+    canva5.create_image(150,100,image=canva1.bg)
+    canva5.create_text(150,100,text="Plante 5")
+    canva5.pack()
+    
+def plante6(Pan):
+    print("plante 6")
+    plt6=Toplevel(Pan)
+    canva6=Canvas(plt6,width=300, height=200)
+    canva6.bg=PhotoImage(file="bgpanneau.png")
+    canva6.create_image(150,100,image=canva6.bg)
+    canva6.create_text(150,100,text="Plante 6")
+    canva6.pack()
+    
+def plante7(Pan):
     print("plante 7")
     
-def plante8(fenetre):
+def plante8(Pan):
     print("plante 8")
 
-def plante9(fenetre):
+def plante9(Pan):
     print("plante 9")
     
-def plante10(fenetre):
+def plante10(Pan):
     print("plante 10")
 
-def plante11(fenetre):
+def plante11(Pan):
     print("plante 11")
     
-def plante12(fenetre):
+def plante12(Pan):
     print("plante 12")
