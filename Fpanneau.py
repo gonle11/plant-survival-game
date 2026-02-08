b9e340178b4e528331bd69c0fa4e61ce09b1cff6
Nathan <nathan.temimevoisin@gmail.com>
2026-02-08 12:58:51 +0100

    mettre des btn à la place des recatngles

--- Fpanneau.py
+++ Fpanneau.py
@@ -8,21 +8,66 @@ def panneau(fenetre):
     canvaPan.create_image(350,250,image=canvaPan.bg)
     canvaPan.create_text(350,35,text="Plantes",font=("Times",50))
     
-    btn1=Button(Pan,text="1",font=("Arial",15),background="#b4b4b4",image="plante1.png")#,command=plante1)
+    #plante 1
+    canvaPan.plante1=PhotoImage(file="plante1.png")
+    btn1=Button(Pan,font=("Arial",15),background="#b4b4b4",image=canvaPan.plante1)#,command=plante1)
     btn1.place(x=75,y=75)
 
-    #canvaPan.create_rectangle(75,75,175,175,fill="#b4b4b4")
-    canvaPan.create_rectangle(215,75,315,175,fill="#b4b4b4")
-    canvaPan.create_rectangle(355,75,455,175,fill="#b4b4b4")
-    canvaPan.create_rectangle(495,75,595,175,fill="#b4b4b4")
-    canvaPan.create_rectangle(75,200,175,300,fill="#b4b4b4")
-    canvaPan.create_rectangle(215,200,315,300,fill="#b4b4b4")
-    canvaPan.create_rectangle(355,200,455,300,fill="#b4b4b4")
-    canvaPan.create_rectangle(495,200,595,300,fill="#b4b4b4")
-    canvaPan.create_rectangle(75,325,175,425,fill="#b4b4b4")
-    canvaPan.create_rectangle(215,325,315,425,fill="#b4b4b4")
-    canvaPan.create_rectangle(355,325,455,425,fill="#b4b4b4")
-    canvaPan.create_rectangle(495,325,595,425,fill="#b4b4b4")
+    #plante 2
+    canvaPan.plante1=PhotoImage(file="plante1.png")
+    btn1=Button(Pan,font=("Arial",15),background="#b4b4b4",image=canvaPan.plante1)#,command=plante1)
+    btn1.place(x=215,y=75)
+
+    #plante 3
+    canvaPan.plante1=PhotoImage(file="plante1.png")
+    btn1=Button(Pan,font=("Arial",15),background="#b4b4b4",image=canvaPan.plante1)#,command=plante1)
+    btn1.place(x=355,y=75)
+
+    #plante 4
+    canvaPan.plante1=PhotoImage(file="plante1.png")
+    btn1=Button(Pan,font=("Arial",15),background="#b4b4b4",image=canvaPan.plante1)#,command=plante1)
+    btn1.place(x=495,y=75)
+
+    #plante 5
+    canvaPan.plante1=PhotoImage(file="plante1.png")
+    btn1=Button(Pan,font=("Arial",15),background="#b4b4b4",image=canvaPan.plante1)#,command=plante1)
+    btn1.place(x=75,y=200)
+
+    #plante 6
+    canvaPan.plante1=PhotoImage(file="plante1.png")
+    btn1=Button(Pan,font=("Arial",15),background="#b4b4b4",image=canvaPan.plante1)#,command=plante1)
+    btn1.place(x=215,y=200)
+
+    #plante 7
+    canvaPan.plante1=PhotoImage(file="plante1.png")
+    btn1=Button(Pan,font=("Arial",15),background="#b4b4b4",image=canvaPan.plante1)#,command=plante1)
+    btn1.place(x=355,y=200)
+
+    #plante 8
+    canvaPan.plante1=PhotoImage(file="plante1.png")
+    btn1=Button(Pan,font=("Arial",15),background="#b4b4b4",image=canvaPan.plante1)#,command=plante1)
+    btn1.place(x=495,y=200)
+
+    #plante 9
+    canvaPan.plante1=PhotoImage(file="plante1.png")
+    btn1=Button(Pan,font=("Arial",15),background="#b4b4b4",image=canvaPan.plante1)#,command=plante1)
+    btn1.place(x=75,y=325)
+
+    #plante 10
+    canvaPan.plante1=PhotoImage(file="plante1.png")
+    btn1=Button(Pan,font=("Arial",15),background="#b4b4b4",image=canvaPan.plante1)#,command=plante1)
+    btn1.place(x=215,y=325)
+
+    #plante 11
+    canvaPan.plante1=PhotoImage(file="plante1.png")
+    btn1=Button(Pan,font=("Arial",15),background="#b4b4b4",image=canvaPan.plante1)#,command=plante1)
+    btn1.place(x=355,y=325)
+    
+    #plante 12
+    canvaPan.plante1=PhotoImage(file="plante1.png")
+    btn1=Button(Pan,font=("Arial",15),background="#b4b4b4",image=canvaPan.plante1)#,command=plante1)
+    btn1.place(x=495,y=325)
+
     canvaPan.pack()
 
     
