0f77a76d61a43a2a0135efc9f28bf85bd388c4a2
Nathan <nathan.temimevoisin@gmail.com>
2026-02-08 13:25:11 +0100

    fonction pour afficher meilleurs regalges plantes pour tt les btn

--- Fpanneau.py
+++ Fpanneau.py
@@ -10,65 +10,88 @@ def panneau(fenetre):
     canvaPan.create_text(350,35,text="Plantes",font=("Times",50))
     
     #plante 1
+    def appel_plante1():
+        Fplantes.plante1(fenetre)
     canvaPan.plante1=PhotoImage(file="plante1.png")
     btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante1,command=appel_plante1)
     btn1.place(x=75,y=75)
-    def appel_plante1():
-        Fplantes.plante1(fenetre)
+    
 
     #plante 2
+    def appel_plante2():
+        Fplantes.plante2(fenetre)
     canvaPan.plante2=PhotoImage(file="plante1.png")
-    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante2)#,command=plante1)
+    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante2,command=appel_plante2)
     btn1.place(x=215,y=75)
 
     #plante 3
+    def appel_plante3():
+        Fplantes.plante3(fenetre)
     canvaPan.plante3=PhotoImage(file="plante1.png")
-    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante3)#,command=plante1)
+    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante3, command=appel_plante3)
     btn1.place(x=355,y=75)
 
     #plante 4
+    def appel_plante4():
+        Fplantes.plante4(fenetre)
     canvaPan.plante4=PhotoImage(file="plante1.png")
-    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante4)#,command=plante1)
+    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante4,command=appel_plante4)
     btn1.place(x=495,y=75)
 
     #plante 5
+    def appel_plante5():
+        Fplantes.plante5(fenetre)
     canvaPan.plante5=PhotoImage(file="plante1.png")
-    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante5)#,command=plante1)
+    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante5,command=appel_plante5)
     btn1.place(x=75,y=200)
 
     #plante 6
+    def appel_plante6():
+        Fplantes.plante6(fenetre)
     canvaPan.plante6=PhotoImage(file="plante1.png")
-    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante6)#,command=plante1)
+    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante6,command=appel_plante6)
     btn1.place(x=215,y=200)
 
     #plante 7
+    def appel_plante7():
+        Fplantes.plante7(fenetre)
     canvaPan.plante7=PhotoImage(file="plante1.png")
-    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante7)#,command=plante1)
+    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante7,command=appel_plante7)
     btn1.place(x=355,y=200)
 
     #plante 8
+    def appel_plante8():
+        Fplantes.plante8(fenetre)
     canvaPan.plante8=PhotoImage(file="plante1.png")
-    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante8)#,command=plante1)
+    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante8,command=appel_plante8)
     btn1.place(x=495,y=200)
 
     #plante 9
+    def appel_plante9():
+        Fplantes.plante9(fenetre)
     canvaPan.plante9=PhotoImage(file="plante1.png")
-    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante9)#,command=plante1)
+    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante9,command=appel_plante9)
     btn1.place(x=75,y=325)
 
     #plante 10
+    def appel_plante10():
+        Fplantes.plante10(fenetre)
     canvaPan.plante10=PhotoImage(file="plante1.png")
-    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante10)#,command=plante1)
+    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante10,command=appel_plante10)
     btn1.place(x=215,y=325)
 
     #plante 11
+    def appel_plante11():
+        Fplantes.plante11(fenetre)
     canvaPan.plante11=PhotoImage(file="plante1.png")
-    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante11)#,command=plante1)
+    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante11,command=appel_plante11)
     btn1.place(x=355,y=325)
     
     #plante 12
+    def appel_plante12():
+        Fplantes.plante12(fenetre)
     canvaPan.plante12=PhotoImage(file="plante1.png")
-    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante12)#,command=plante1)
+    btn1=Button(Pan,font=("Arial",15),image=canvaPan.plante12,command=appel_plante12)
     btn1.place(x=495,y=325)
 
     canvaPan.pack()
--- Fplantes.py
+++ Fplantes.py
@@ -2,3 +2,36 @@ from tkinter import *
 
 def plante1(fenetre):
     print("plante 1")
+    
+def plante2(fenetre):
+    print("plante 2")
+
+def plante3(fenetre):
+    print("plante 3")
+    
+def plante4(fenetre):
+    print("plante 4")
+    
+def plante5(fenetre):
+    print("plante 5")
+    
+def plante6(fenetre):
+    print("plante 6")
+
+def plante7(fenetre):
+    print("plante 7")
+    
+def plante8(fenetre):
+    print("plante 8")
+
+def plante9(fenetre):
+    print("plante 9")
+    
+def plante10(fenetre):
+    print("plante 10")
+
+def plante11(fenetre):
+    print("plante 11")
+    
+def plante12(fenetre):
+    print("plante 12")
