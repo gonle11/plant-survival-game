a8db0599db14a8e0d9bfd91589d56cfa9a425147
Nathan <nathan.temimevoisin@gmail.com>
2026-02-09 20:53:03 +0100

    création fonction

--- Finfoplt.py
+++ Finfoplt.py
@@ -1,28 +1,32 @@
 from tkinter import *
 
-def infoplt1(fenetre):
+def canva (fenetre):
     Plt=Toplevel(fenetre,width=500,height=500)
+
     canvaPlt=Canvas(Plt,width=500,height=300)
     canvaPlt.bg=PhotoImage(file="bgpanneau.png")
     canvaPlt.create_image(350,250,image=canvaPlt.bg)
 
-    
     valueHum = IntVar()
     scaleHum=Scale(Plt,variable=valueHum,orient=HORIZONTAL,bg="peru")
     Humid=valueHum.get()
     scaleHum.place(x=300,y=50)
-    canvaPlt.create_text(100,50,text="Humidité : ")
+    canvaPlt.create_text(133,75,text="Humidité : ",font=("Times",35))
 
     valueLum = IntVar()
     scaleLum=Scale(Plt,variable=valueLum,orient=HORIZONTAL,bg="peru")
     Lum=valueLum.get()
     scaleLum.place(x=300,y=100)
-    canvaPlt.create_text(100,100,text="Luminosité : ")
+    canvaPlt.create_text(150,120,text="Luminosité : ",font=("Times",35))
 
     valueChal = IntVar()
     scaleChal=Scale(Plt,variable=valueChal,orient=HORIZONTAL,bg="peru")
     Chal=valueChal.get()
     scaleChal.place(x=300,y=150)
-    canvaPlt.create_text(100,150,text="Chaleur : ")
+    canvaPlt.create_text(120,175,text="Chaleur : ",font=("Times",35))
 
     canvaPlt.pack()
+    return Humid, Lum, Chal
+
+def infoplt1(fenetre):
+    print(canva(fenetre))
