92f6c643b26975d684948a9be11986d3a2966186
Nathan <nathan.temimevoisin@gmail.com>
2026-02-09 20:06:52 +0100

    création scrollbar

--- Finfoplt.py
+++ Finfoplt.py
@@ -1,8 +1,23 @@
 from tkinter import *
 
 def infoplt1(fenetre):
-    Plt=Toplevel(fenetre)
+    Plt=Toplevel(fenetre,width=500,height=500)
     canvaPlt=Canvas(Plt,width=500,height=300)
     canvaPlt.bg=PhotoImage(file="bgpanneau.png")
     canvaPlt.create_image(350,250,image=canvaPlt.bg)
+
+    
+    valueHum = IntVar()
+    scaleHum=Scrollbar(Plt,orient=HORIZONTAL,bg="peru")
+    Humid=valueHum.get()
+    scaleHum.place(x=25,y=25)
+
+    valueLum = DoubleVar()
+    scaleLum=Scrollbar(Plt)
+    scaleLum.pack(anchor=CENTER)#lace(x=25,y=50)
+
+    valueChal = DoubleVar()
+    scaleChal=Scrollbar(Plt)
+    scaleChal.pack()#lace(x=25,y=75)
+
     canvaPlt.pack()
