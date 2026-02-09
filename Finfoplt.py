da8897515a8e690c97a08cbf1df224852174051e
Nathan <nathan.temimevoisin@gmail.com>
2026-02-09 21:05:35 +0100

    création btn pour recup valeurs

--- Finfoplt.py
+++ Finfoplt.py
@@ -1,6 +1,23 @@
 from tkinter import *
 
+def recup_hum():
+    global Humid
+    Humid=valueHum.get()
+    print(Humid)
+
+
+def recup_lum():
+    global Lumin
+    Lumin=valueLum.get()
+    print(Lumin)
+
+def recup_chal():
+    global Chal
+    Chal=valueChal.get()
+    print(Chal)
+
 def canva (fenetre):
+    global valueHum, valueLum, valueChal
     Plt=Toplevel(fenetre,width=500,height=500)
 
     canvaPlt=Canvas(Plt,width=500,height=300)
@@ -9,24 +26,26 @@ def canva (fenetre):
 
     valueHum = IntVar()
     scaleHum=Scale(Plt,variable=valueHum,orient=HORIZONTAL,bg="peru")
-    Humid=valueHum.get()
     scaleHum.place(x=300,y=50)
     canvaPlt.create_text(133,75,text="Humidité : ",font=("Times",35))
+    btnHum=Button(Plt,text="Valider",background="peru",command=recup_hum)
+    btnHum.place(x=450,y=50)
 
     valueLum = IntVar()
     scaleLum=Scale(Plt,variable=valueLum,orient=HORIZONTAL,bg="peru")
-    Lum=valueLum.get()
     scaleLum.place(x=300,y=100)
     canvaPlt.create_text(150,120,text="Luminosité : ",font=("Times",35))
+    btnLum=Button(Plt,text="Valider",background="peru",command=recup_lum)
+    btnLum.place(x=450,y=100)
 
     valueChal = IntVar()
     scaleChal=Scale(Plt,variable=valueChal,orient=HORIZONTAL,bg="peru")
-    Chal=valueChal.get()
     scaleChal.place(x=300,y=150)
     canvaPlt.create_text(120,175,text="Chaleur : ",font=("Times",35))
+    btnChal=Button(Plt,text="Valider",background="peru",command=recup_chal)
+    btnChal.place(x=450,y=150)
 
     canvaPlt.pack()
-    return Humid, Lum, Chal
 
 def infoplt1(fenetre):
-    print(canva(fenetre))
+    canva(fenetre)
