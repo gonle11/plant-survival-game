e7d4214e4c6ca768f88e4b2a48193aa93f462df0
Nathan <nathan.temimevoisin@gmail.com>
2026-02-09 14:16:43 +0100

    creation canva

--- Finfoplt.py
+++ Finfoplt.py
@@ -1,4 +1,8 @@
 from tkinter import *
 
 def infoplt1(fenetre):
-    Plt1=Toplevel(fenetre)
+    Plt=Toplevel(fenetre)
+    canvaPlt=Canvas(Plt,width=500,height=300)
+    canvaPlt.bg=PhotoImage(file="bgpanneau.png")
+    canvaPlt.create_image(350,250,image=canvaPlt.bg)
+    canvaPlt.pack()
