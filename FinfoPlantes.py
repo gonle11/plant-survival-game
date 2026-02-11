32bdcbf76def39f4d424fd7210fd7e948c459faf
Nathan <nathan.temimevoisin@gmail.com>
2026-02-11 18:21:32 +0100

    création btn + fonction après

--- FinfoPlantes.py
+++ FinfoPlantes.py
@@ -3,3 +3,8 @@ from tkinter import font
 
 def infoPlantes(fenetre):
         InfoPlantes = Toplevel(fenetre, width=800, height=500,background="grey")
+        btn1 = Button(InfoPlantes, text="Epipremnum aureum ",command=plt1)
+        btn1.place(x=25,y=25)
+
+def plt1():
+        print("plt1")
