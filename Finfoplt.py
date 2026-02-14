4f45966e8aefab2f181d45322a51ddc402fc88fd
Nathan <nathan.temimevoisin@gmail.com>
2026-02-14 18:10:04 +0100

    appeller plaquette mini-explication sur quels sont les plantes à partir des reglages

--- Finfoplt.py
+++ Finfoplt.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import FinfoPlantes
 
 def recup_hum(Humid):
     print(Humid)
@@ -16,8 +17,9 @@ def ajt_eau():
     print(eauActu)
     canvaPlt.itemconfigure("eau",text="(actuelement : {} cl)".format(eauActu))
 
-def canva (fenetre,titre,lumi,humi,chal):
-    global valueHum, valueLum, valueChal,canvaPlt
+def canva (fenetre,titre,lumi,humi,chal,fct):
+    global valueHum, valueLum, valueChal,canvaPlt,Plt    
+        
     Plt=Toplevel(fenetre,width=500,height=500)
 
     canvaPlt=Canvas(Plt,width=500,height=300)
@@ -25,6 +27,10 @@ def canva (fenetre,titre,lumi,humi,chal):
     canvaPlt.create_image(350,250,image=canvaPlt.bg)
 
     canvaPlt.create_text(250,30,text=titre,font=("Times",50))
+
+    btnInfo=Button(Plt,text="*",bg="orange",bd=0,command=fct)
+    btnInfo.place(x=450,y=25)
+
 
     valueHum = IntVar()
     scaleHum=Scale(Plt,variable=valueHum,orient=HORIZONTAL,bg="peru",command=recup_hum)
@@ -54,59 +60,84 @@ def canva (fenetre,titre,lumi,humi,chal):
 def infoplt1(fenetre):
     global eauActu
     eauActu=50
-    canva(fenetre,"plante 1",55,50,55)
+    canva(fenetre,"plante 1",55,50,55,Aplt1)
 
 def infoplt2(fenetre):
     global eauActu
     eauActu=40
-    canva(fenetre,"plante 2",55,30,55)
+    canva(fenetre,"plante 2",55,30,55,Aplt2)
 
 def infoplt3(fenetre):
     global eauActu
     eauActu=55
-    canva(fenetre,"plante 3",65,50,55)
+    canva(fenetre,"plante 3",65,50,55,Aplt3)
 
 def infoplt4(fenetre):
     global eauActu
     eauActu=60
-    canva(fenetre,"plante 4",70,60,60)
+    canva(fenetre,"plante 4",70,60,60,Aplt4)
 
 def infoplt5(fenetre):
     global eauActu
     eauActu=65
-    canva(fenetre,"plante 5",70,70,65)
+    canva(fenetre,"plante 5",70,70,65,Aplt5)
 
 def infoplt6(fenetre):
     global eauActu
     eauActu=60
-    canva(fenetre,"plante 6",50,80,60)
+    canva(fenetre,"plante 6",50,80,60,Aplt6)
 
 def infoplt7(fenetre):
     global eauActu
     eauActu=55
-    canva(fenetre,"plante 7",60,50,60)
+    canva(fenetre,"plante 7",60,50,60,Aplt7)
 
 def infoplt8(fenetre):
     global eauActu
     eauActu=65
-    canva(fenetre,"plante 8",60,90,65)
+    canva(fenetre,"plante 8",60,90,65,Aplt8)
 
 def infoplt9(fenetre):
     global eauActu
     eauActu=70
-    canva(fenetre,"plante 9",70,90,70)
+    canva(fenetre,"plante 9",70,90,70,Aplt9)
 
 def infoplt10(fenetre):
     global eauActu
     eauActu=60
-    canva(fenetre,"plante 10",80,60,65)
+    canva(fenetre,"plante 10",80,60,65,Aplt10)
 
 def infoplt11(fenetre):
     global eauActu
     eauActu=25
-    canva(fenetre,"plante 11",95,15,80)
+    canva(fenetre,"plante 11",95,15,80,Aplt11)
 
 def infoplt12(fenetre):
     global eauActu
     eauActu=75
-    canva(fenetre,"plante 12",80,95,75)
+    canva(fenetre,"plante 12",80,95,75,Aplt12)
+
+def Aplt1():
+    FinfoPlantes.plt1(Plt)
+def Aplt2():
+    FinfoPlantes.plt2(Plt)
+def Aplt3():
+    FinfoPlantes.plt3(Plt)
+def Aplt4():
+    FinfoPlantes.plt4(Plt)
+def Aplt5():
+    FinfoPlantes.plt5(Plt)
+def Aplt6():
+    FinfoPlantes.plt6(Plt)
+def Aplt7():
+    FinfoPlantes.plt7(Plt)
+def Aplt8():
+    FinfoPlantes.plt8(Plt)
+def Aplt9():
+    FinfoPlantes.plt9(Plt)
+def Aplt10():
+    FinfoPlantes.plt10(Plt)
+def Aplt11():
+    FinfoPlantes.plt11(Plt)
+def Aplt12():
+    FinfoPlantes.plt12(Plt)
